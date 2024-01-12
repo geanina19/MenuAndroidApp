345f4bc17bc552956f8bebb16978be9a0a8a27cd
Geanina <geany849@gmail.com>
2024-01-12 08:51:11 +0100

    ns

--- app/src/main/java/com/example/menusapp/EjemploMenuContextualActivity.kt
+++ app/src/main/java/com/example/menusapp/EjemploMenuContextualActivity.kt
@@ -1,7 +1,11 @@
 package com.example.menusapp
 
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.View
 import com.example.menusapp.databinding.ActivityEjemploMenuContextualBinding
 
 class EjemploMenuContextualActivity : AppCompatActivity() {
@@ -12,5 +16,36 @@ class EjemploMenuContextualActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEjemploMenuContextualBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        when (v?.id)
+        {
+            binding.myBoton.id -> {
+                menuInflate.inflate(R.menu.colores_menu, menu)
+            }
+
+            binding.otroBoton.id -> {
+                menuInflate.inflate(R.menu.colores_menu, menu)
+            }
+        }
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.redOption -> {
+                item.actionView?.setBackgroundColor(Color.RED)
+            }
+
+            R.id.redOption -> {
+                item.actionView?.setBackgroundColor(Color.RED)
+            }
+        }
     }
 }
